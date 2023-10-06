fc7522fb53a545a31f15b07e46b8d55f0e3b0821
Greegko <greegko+github@gmail.com>
2023-10-06 15:55:45 +0200

    fix plugins buildability

--- packages/_plugin-skeleton/src/module.ts
+++ packages/_plugin-skeleton/src/module.ts
@@ -2,7 +2,7 @@ import { Module } from "@greegko/core";
 
 import { MainPage } from "./components/pages/main";
 
-export const creditSystemModule: Module = {
+export const pluginSkeletonModule: Module = {
   name: "Plugin Skeleton",
   icon: "question-octagon",
   url: "plugin-skeleton",
