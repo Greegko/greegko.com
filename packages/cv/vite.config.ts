5f42d891884bfb56e83252139cec1270577bdbf7
Greegko <greegko+github@gmail.com>
2025-07-03 17:43:49 +0200

    remove base url

--- packages/cv/vite.config.ts
+++ packages/cv/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  base: "/cv",
   plugins: [solid(), tailwindcss()],
   server: {
     port: 8080,
